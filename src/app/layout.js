52c7ea1a2e1b758769ad9583e50800f803f82246
Shashank <shashanke7y@gmail.com>
2023-12-27 19:42:02 -0500

    added cuthbertson favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="./Cuthbertson_Favicon.png" sizes="any" />
       <body className={league.className}>{children}</body>
     </html>
   );
