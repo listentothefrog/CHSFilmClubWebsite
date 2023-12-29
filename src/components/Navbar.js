cdb984d5d2aae9bb5dc209b94a8643a2ac34f8a8
Shashank <shashanke7y@gmail.com>
2023-12-29 12:16:11 -0500

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,115 @@
-import React from "react";
-import Image from "next/image";
 import Link from "next/link";
-export default function Navbar() {
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Image from "next/image";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link_href: "/",
+      link: "home",
+    },
+    {
+      id: 2,
+      link_href: "/aboutus",
+      link: "about us",
+    },
+    {
+      id: 3,
+      link_href: "/newsroom",
+      link: "newsroom",
+    },
+    {
+      id: 4,
+      link_href: "/ourfilms",
+      link: "our films",
+    },
+    {
+      id: 5,
+      link_href: "/filmfestival",
+      link: "film festival",
+    },
+  ];
+
   return (
-    <div className="flex items-center justify-between w-full h-20 pl-2 pr-2">
+    <div className="flex justify-between items-center w-full h-20 px-4 fixed nav">
       <div className="flex items-center">
         <Image
           src={"/Cuthbertson_Favicon.png"}
-          width={35}
-          height={30}
+          width={42}
+          height={40}
           alt="Cuthbertson Logo"
         />
-        <div className="ml-2">
-          <h1 className="text-xl uppercase font-bold">Cuthbertson Film Club</h1>
-        </div>
+        <h1 className="text-2xl font-signature ml-2 font-bold">
+          Cuthbertson Film Club
+        </h1>
       </div>
-      <div className="flex items-center">
-        <div>
+
+      <ul className="hidden md:flex md:items-center">
+        <li className="px-4 cursor-pointer capitalize font-medium link-underline">
           <Link href={"/"}>Home</Link>
-        </div>
-        <div className="ml-6">About Us</div>
-        <div className="ml-6">Newsroom</div>
-        <div className="ml-6">Our Films</div>
-        <div className="ml-6">Film Festival</div>
-        <div className="ml-6">
-          <button className="px-4 py-2 bg-filmcluborange rounded-md font-semibold text-white">
-            Join Film Club
-          </button>
-        </div>
+        </li>
+        <li className="px-4 cursor-pointer capitalize font-medium link-underline">
+          <Link href={"/aboutus"}>About Us</Link>
+        </li>
+        <li className="px-4 cursor-pointer capitalize font-medium link-underline">
+          <Link href={"/newsroom"}>Newsroom</Link>
+        </li>
+        <li className="px-4 cursor-pointer capitalize font-medium link-underline">
+          <Link href={"/ourfilms"}>Our Films</Link>
+        </li>
+        <li className="px-4 cursor-pointer capitalize font-medium link-underline">
+          <Link href={"/filmfestival"}>Film Festival</Link>
+        </li>
+        <li className="px-4 cursor-pointer capitalize font-medium link-underline">
+          <Link href={"/joinfilmclub"}>
+            <button className="px-4 py-2 bg-filmcluborange rounded-md font-semibold text-white">
+              Join Film Club
+            </button>
+          </Link>
+        </li>
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 md:hidden"
+      >
+        {nav ? (
+          <FaTimes className="text-black" size={30} />
+        ) : (
+          <FaBars className="text-black" size={30} />
+        )}
       </div>
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-white to-gray-300 text-black">
+          <li className="px-4 cursor-pointer capitalize py-6 text-2xl">
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li className="px-4 cursor-pointer capitalize py-6 text-2xl">
+            <Link href={"/aboutus"}>About Us</Link>
+          </li>
+          <li className="px-4 cursor-pointer capitalize py-6 text-2xl">
+            <Link href={"/newsroom"}>Newsroom</Link>
+          </li>
+          <li className="px-4 cursor-pointer capitalize py-6 text-2xl">
+            <Link href={"/ourfilms"}>Our Films</Link>
+          </li>
+          <li className="px-4 cursor-pointer capitalize py-6 text-2xl">
+            <Link href={"/filmfestival"}>Film Festival</Link>
+          </li>
+          <li className="px-4 cursor-pointer capitalize py-6 text-2xl">
+            <Link href={"/joinfilmclub"}>
+              <button className="px-4 py-2 bg-filmcluborange rounded-md font-semibold text-white">
+                Join Film Club
+              </button>
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
-}
+};
+
+export default Navbar;
