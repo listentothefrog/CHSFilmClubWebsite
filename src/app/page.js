7ecabe8d98c2fcc076fb4b763f3d8ff4efc7c02a
Shashank <shashanke7y@gmail.com>
2024-01-01 21:22:11 -0500

    revamped hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,38 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
-import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full h-screen">
       <Navbar />
-      <div class="max-w-full px-4 sm:px-6 lg:px-8 max-h-3.5">
-        <div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
-          <div>
-            <h1 class="block text-3xl font-bold text-black sm:text-4xl lg:text-6xl lg:leading-tight">
-              WELCOME TO THE CHS FILM CLUB.
-            </h1>
-            <p class="mt-3 text-lg text-gray-800">
+      <div className="w-full md:px-8 flex items-center flex-col px-6 md:flex-row">
+        <div className="md:w-2/4 md:mr-10">
+          <div className="text-2xl font-bold md:text-5xl">
+            <h1>WELCOME TO THE CHS FILM CLUB.</h1>
+          </div>
+          <div className="mt-4">
+            <p className="text-md text-gray-600 md:text-lg">
               Welcome to the Cuthbertson High School Film Club, where your
               passion for storytelling takes center stage! Our club is a dynamic
               community of creative minds, cinephiles, and aspiring filmmakers
               who share a deep love for the art of filmmaking. Whether you're a
               seasoned filmmaker or just beginning to explore the world of
-              movies, there's a place for you here.
+              movies, there's a place for you here.{" "}
             </p>
-
-            <div class="mt-7 grid gap-3 w-full sm:inline-flex">
-              <Link
-                class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-filmcluborange text-white hover:bg-orange-600"
-                href="/joinfilmclub"
-              >
+          </div>
+          <div className="mt-4 w-full flex flex-col">
+            <Link href={"/joinfilmclub"}>
+              <button className="bg-filmcluborange py-2 rounded-md text-white font-medium">
                 Join Film Club
-                <svg
-                  class="flex-shrink-0 w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="m9 18 6-6-6-6" />
-                </svg>
-              </Link>
-              <Link
-                class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border-2 border-filmcluborange shadow-sm "
-                href="#"
-              >
-                Learn More
-              </Link>
-            </div>
+              </button>
+            </Link>
+            <button className="mt-2 border-2 border-filmcluborange py-2 rounded-md text-black font-medium">
+              Learn More
+            </button>
           </div>
-
-          <div class="ms-4">
-            <Image
-              class="md:w-3/4"
-              src="/Film-rolls-pana.svg"
-              width={600}
-              height={600}
-              alt="Image Description"
-            />
-          </div>
+        </div>
+        <div className="mt-4 md:w-1/2">
+          <img src="https://imgix.bustle.com/inverse/94/8a/38/6b/0df2/4e7b/8bdd/3a5592fffb2f/6824890-jurassic-parkjpg.jpeg?w=1200&h=630&fit=crop&crop=faces&fm=jpg" />
         </div>
       </div>
     </div>
