913ab63552c80d84b8d8cfaac33318345d1b9d96
Shashank <shashanke7y@gmail.com>
2024-01-01 21:26:34 -0500

    changed image

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
           </div>
           <div className="mt-4 w-full flex flex-col">
             <Link href={"/joinfilmclub"}>
-              <button className="bg-filmcluborange py-2 rounded-md text-white font-medium">
+              <button className="w-full bg-filmcluborange py-2 rounded-md text-white font-medium">
                 Join Film Club
               </button>
             </Link>
@@ -31,8 +31,11 @@ export default function Home() {
             </button>
           </div>
         </div>
-        <div className="mt-4 md:w-1/2">
-          <img src="https://imgix.bustle.com/inverse/94/8a/38/6b/0df2/4e7b/8bdd/3a5592fffb2f/6824890-jurassic-parkjpg.jpeg?w=1200&h=630&fit=crop&crop=faces&fm=jpg" />
+        <div className="mt-4 md:w-1/2 md:h-1/4">
+          <img
+            alt="Video production group"
+            src="https://images.unsplash.com/photo-1632187989763-c9c620420b4d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
         </div>
       </div>
     </div>
